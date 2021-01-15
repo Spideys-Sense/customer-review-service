611ba7faddbc07f6acb994c40a814510f0d5f6e0
robeusanio11 <robeusanio11@gmail.com>
2021-01-14 16:06:26 -0800

    server/client connected

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const db = require('../database/index');
 const {Reviews, Items} = require('../database/Model');
+const path = require('path');
 
 const PORT = 1111;
+
+app.use(express.static(path.join(__dirname, '../client/public')))
 
 app.listen(PORT, () => {
   console.log('Server listening on port: ' + PORT);
