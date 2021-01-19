ae873db6edb50a06e8491aaa89c5d33199664cb9
robeusanio11 <robeusanio11@gmail.com>
2021-01-18 20:32:16 -0800

    added header for review list

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -8,11 +8,46 @@ const StyledReviewList = styled.div`
   border-style: solid;
 `
 
-let ReviewList = ({reviews}) => (
+const FilterByDropDown = () => (
+  <select>
+    <option value={[1, 2, 3, 4, 5]}>All stars</option>
+    <option value={5}>5 stars only</option>
+    <option value={4}>4 stars only</option>
+    <option value={3}>3 stars only</option>
+    <option value={2}>2 stars only</option>
+    <option value={1}>1 stars only</option>
+    <option value={[4, 5]}>All positive</option>
+    <option value={[1, 2, 3]}>All critical</option>
+  </select>
+);
+
+const SortByDropDown = () => (
+  <select>
+    <option value='newest'>Newest</option>
+    <option value='oldest'>Oldest</option>
+    <option value='highest_rating'>Highest rating</option>
+    <option value='lowest_rating'>Lowest rating</option>
+  </select>
+)
+
+const ReviewList = ({ reviews }) => (
   <StyledReviewList>
-    {reviews.map((review) => (
-      <ReviewListEntry review={review} />
-    ))}
+    <header>
+      <h4>{reviews.length} Reviews</h4>
+        <div>
+          <p>Showing All Reviews</p>
+          <p>Filter by: </p>
+          <FilterByDropDown/>
+          <p>Sort by: </p>
+          <SortByDropDown/>
+        </div>
+    </header>
+    <main>
+
+      {reviews.map((review) => (
+        <ReviewListEntry review={review} />
+      ))}
+    </main>
   </StyledReviewList>
 );
 
