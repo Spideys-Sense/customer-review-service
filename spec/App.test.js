97d145c20c3f06e824134e528c2ae6ad481131b9
robeusanio11 <robeusanio11@gmail.com>
2021-01-18 17:16:40 -0800

    tests now cleaner

--- spec/App.test.js
+++ spec/App.test.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { expect } from 'chai';
+
 import App from '../client/src/components/App.jsx';
 import ReviewAverage from '../client/src/components/ReviewAverage.jsx';
 import ReviewList from '../client/src/components/ReviewList.jsx';
 import WriteReview from '../client/src/components/WriteReview.jsx';
 import PhotoGallery from '../client/src/components/PhotoGallery.jsx';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'chai';
 
 Enzyme.configure({adapter: new Adapter()});
 
-test('should contain 4 components', () => {
-  const wrapper = Enzyme.shallow(<App/>);
-  expect(wrapper.contains(<ReviewAverage/>)).to.be.true;
-  expect(wrapper.contains(<ReviewList/>)).to.be.true;
-  expect(wrapper.contains(<WriteReview/>)).to.be.true;
-  expect(wrapper.contains(<PhotoGallery/>)).to.be.true;
+describe('<App/>', () => {
+  test('should contain all 4 components', () => {
+    const wrapper = Enzyme.shallow(<App/>);
+    const components = [<ReviewAverage/>, <ReviewList/>, <WriteReview/>, <PhotoGallery/>];
+    expect(wrapper.containsAllMatchingElements(components));
+  });
 })
