681b2836ecd7c58a9b767b4b75cd03dc97d7701b
robeusanio11 <robeusanio11@gmail.com>
2021-01-21 14:13:46 -0800

    more css added for average reviews component

--- client/src/components/ReviewAverage.jsx
+++ client/src/components/ReviewAverage.jsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 
 const StyledReviewAverage = styled.div`
   grid-row: 1;
-  grid-column: 1 / span 3;
-  border-style: solid;
+  grid-column: 1 / span 2;
   font-family: Roboto, serif;
 `;
 
@@ -19,33 +18,40 @@ const StyledReviewRatingGraph = styled.div`
   display: grid;
   grid-template-columns: 1fr 5fr 1fr;
   grid-template-rows: repeat(5, 1fr);
+  margin-bottom: 10px;
 `
 
 const StyledStarBar = styled.div`
   width: ${props => props.barSize};
-  height: 18px;
+  height: 15px;
   background-color: orange;
   border-radius: 3px;
+  margin-top: auto;
+  margin-bottom: auto;
 `
+
+const StyledStarGraphText = styled.div`
+  font-size: 13px;
+  margin-top: 3px;
+  margin-bottom: 3px;
+`;
 
 const ReviewAverage = ({ reviews, averages }) => (
   <StyledReviewAverage>
-    <h2>Customer Reviews</h2>
+    <h2 style={{'margin-top':'0px'}}>Customer Reviews</h2>
     <StyledReviewAverageSubheader>
-      <div>stars</div>
+      <div>IMAGE HERE</div>
       <div style={{'color':'#2e5ee2'}}>{reviews.length} Reviews</div>
       <div>{averages[0]} out of 5 stars</div>
     </StyledReviewAverageSubheader>
     <div>
-      <p>Filter reviews by star rating</p>
+      <h5 style={{'margin-bottom':'5px'}}><b>Filter reviews by star rating</b></h5>
       <StyledReviewRatingGraph>
-      <div>5</div><StyledStarBar barSize={averages[5]}></StyledStarBar><div>{averages[5]}</div>
-      <div>4</div><StyledStarBar barSize={averages[4]}></StyledStarBar><div>{averages[4]}</div>
-      <div>3</div><StyledStarBar barSize={averages[3]}></StyledStarBar><div>{averages[3]}</div>
-      <div>2</div><StyledStarBar barSize={averages[2]}></StyledStarBar><div>{averages[2]}</div>
-      <div>1</div><StyledStarBar barSize={averages[1]}></StyledStarBar><div>{averages[1]}</div>
-      <div>hello</div>
-      <div>hello</div>
+        <StyledStarGraphText>5 star</StyledStarGraphText> <StyledStarBar barSize={averages[5]}></StyledStarBar> <StyledStarGraphText>{averages[5]}</StyledStarGraphText>
+        <StyledStarGraphText>4 star</StyledStarGraphText> <StyledStarBar barSize={averages[4]}></StyledStarBar> <StyledStarGraphText>{averages[4]}</StyledStarGraphText>
+        <StyledStarGraphText>3 star</StyledStarGraphText> <StyledStarBar barSize={averages[3]}></StyledStarBar> <StyledStarGraphText>{averages[3]}</StyledStarGraphText>
+        <StyledStarGraphText>2 star</StyledStarGraphText> <StyledStarBar barSize={averages[2]}></StyledStarBar> <StyledStarGraphText>{averages[2]}</StyledStarGraphText>
+        <StyledStarGraphText>1 star</StyledStarGraphText> <StyledStarBar barSize={averages[1]}></StyledStarBar> <StyledStarGraphText>{averages[1]}</StyledStarGraphText>
       </StyledReviewRatingGraph>
     </div>
   </StyledReviewAverage>
